55a1a820407f4289edffbf0328a3ea623c680980
George Reilly <george@reilly.org>
2023-01-02 15:48:25 -0800

    Walk . recursively

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use walkdir::{DirEntry, WalkDir};
+
+fn is_not_hidden(entry: &DirEntry) -> bool {
+    entry
+         .file_name()
+         .to_str()
+         .map(|s| entry.depth() == 0 || !s.starts_with("."))
+         .unwrap_or(false)
+}
+
 fn main() {
-    println!("Hello, world!");
+    WalkDir::new(".")
+        .into_iter()
+        .filter_entry(|e| is_not_hidden(e))
+        .filter_map(|v| v.ok())
+        .for_each(|x| println!("{}", x.path().display()));
 }
