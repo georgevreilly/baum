1a131fa676bc17e7dbcabf0eacb583006cfdfea5
George Reilly <george@reilly.org>
2023-01-02 16:30:59 -0800

    First attempt at tree indentation

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,24 @@ use walkdir::{DirEntry, WalkDir};
 
 fn is_not_hidden(entry: &DirEntry) -> bool {
     entry
-         .file_name()
-         .to_str()
-         .map(|s| entry.depth() == 0 || !s.starts_with("."))
-         .unwrap_or(false)
+        .file_name()
+        .to_str()
+        .map(|s| entry.depth() == 0 || !s.starts_with("."))
+        .unwrap_or(false)
 }
 
 fn main() {
-    WalkDir::new(".")
+    let root = ".";
+    WalkDir::new(root)
         .into_iter()
         .filter_entry(|e| is_not_hidden(e))
         .filter_map(|v| v.ok())
-        .for_each(|x| println!("{}", x.path().display()));
+        .for_each(|x| {
+            let indent = if x.depth() > 0 {
+                "│   ".repeat(x.depth() - 1)
+            } else {
+                "".to_owned()
+            };
+            println!("{}├── {}", indent, x.path().display())
+        });
 }
