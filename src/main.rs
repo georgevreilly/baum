c40d22dcf3871f08bfbd50f94d536a8ff7c9a799
George V. Reilly <george@reilly.org>
2023-01-04 20:58:02 -0800

    use fs::read_dir

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::io;
+use std::fs;
+use std::path::PathBuf;
+
 use walkdir::{DirEntry, WalkDir};
 
 fn is_not_hidden(entry: &DirEntry) -> bool {
@@ -8,8 +12,7 @@ fn is_not_hidden(entry: &DirEntry) -> bool {
         .unwrap_or(false)
 }
 
-fn main() {
-    let root = ".";
+fn walkdir_tree(root: &PathBuf) {
     WalkDir::new(root)
         .into_iter()
         .filter_entry(|e| is_not_hidden(e))
@@ -22,4 +25,23 @@ fn main() {
             };
             println!("{}├── {}", indent, x.path().display())
         });
+}
+
+fn walk(root: &PathBuf) -> io::Result<()> {
+    for e in fs::read_dir(root)? {
+        let e = e?;
+        let path = e.path();
+        println!("{:?}", e);
+        if path.is_dir() {
+            walk(&path)?;
+        }
+    }
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
+    let root = PathBuf::from(".");
+    walk(&root)?;
+    walkdir_tree(&root);
+    Ok(())
 }
