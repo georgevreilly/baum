4e0c7c41dee556ea93bd70f3c99b255626838229
George V. Reilly <george@reilly.org>
2023-01-04 22:12:34 -0800

    Entry enum

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,35 @@
 use std::io;
 use std::fs;
 
-fn walk(root: &str, prefix: &str) -> io::Result<()> {
-    for e in fs::read_dir(root)? {
-        let path = e?.path();
+#[derive(Debug)]
+enum Entry {
+    Directory((String, Vec<Entry>)),
+    File(String), // TODO: metadata
+    Symlink(String),
+}
+
+fn walk(root: &str, prefix: &str) -> io::Result<Entry> {
+    let entries: Vec<fs::DirEntry> = fs::read_dir(root)?.map(|e| e.unwrap()).collect();
+    let mut directory: Vec<Entry> = Vec::with_capacity(entries.len());
+    for e in entries {
+        let path = e.path();
         let name = path.file_name().unwrap().to_str().unwrap();
-        println!("{}├── {}", prefix, name);
+        // println!("{}├── {}", prefix, name);
         if path.is_dir() {
-            walk(&format!("{}/{}", root, name), &format!("{}├  ", prefix))?;
+            let sub = walk(&format!("{}/{}", root, name), &format!("{}├  ", prefix))?;
+            directory.push(sub);
+        } else if path.is_symlink() {
+            directory.push(Entry::Symlink(name.into()));
+        } else {
+            directory.push(Entry::File(name.into()));
         }
     }
-    Ok(())
+    Ok(Entry::Directory((root.into(), directory)))
 }
 
 fn main() -> io::Result<()> {
     let root = ".";
-    walk(&root, "")?;
+    let tree = walk(&root, "")?;
+    println!("{:?}", tree);
     Ok(())
 }
