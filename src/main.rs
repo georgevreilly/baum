2658f3c5594c687130d0356accd21361bcffb37f
George V. Reilly <george@reilly.org>
2023-01-04 22:37:33 -0800

    Use match and PathBuf

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use std::io;
+use std::ffi::OsStr;
 use std::fs;
+use std::io;
+use std::path::PathBuf;
 
 #[derive(Debug)]
 enum Entry {
@@ -8,28 +10,35 @@ enum Entry {
     Symlink(String),
 }
 
-fn walk(root: &str, prefix: &str) -> io::Result<Entry> {
+fn walk(root: &PathBuf, prefix: &str) -> io::Result<Entry> {
     let entries: Vec<fs::DirEntry> = fs::read_dir(root)?.map(|e| e.unwrap()).collect();
     let mut directory: Vec<Entry> = Vec::with_capacity(entries.len());
     for e in entries {
         let path = e.path();
-        let name = path.file_name().unwrap().to_str().unwrap();
+        let name: String = path.file_name().unwrap().to_str().unwrap().into();
+        if name.starts_with(".") {
+            continue;
+        };
         // println!("{}├── {}", prefix, name);
-        if path.is_dir() {
-            let sub = walk(&format!("{}/{}", root, name), &format!("{}├  ", prefix))?;
-            directory.push(sub);
-        } else if path.is_symlink() {
-            directory.push(Entry::Symlink(name.into()));
-        } else {
-            directory.push(Entry::File(name.into()));
-        }
+        let e2 = match path {
+            path if path.is_dir() => walk(&root.join(name), &format!("{}├  ", prefix))?,
+            path if path.is_symlink() => Entry::Symlink(name.into()),
+            _ => Entry::File(name.into()),
+        };
+        directory.push(e2);
     }
-    Ok(Entry::Directory((root.into(), directory)))
+    let name = root
+        .file_name()
+        .unwrap_or(OsStr::new("."))
+        .to_str()
+        .unwrap()
+        .into();
+    Ok(Entry::Directory((name, directory)))
 }
 
 fn main() -> io::Result<()> {
     let root = ".";
-    let tree = walk(&root, "")?;
-    println!("{:?}", tree);
+    let tree = walk(&root.into(), "")?;
+    println!("{:#?}", tree);
     Ok(())
 }
