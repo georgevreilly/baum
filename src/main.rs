08c92d941cba114fab4d0717a856be572ee3c06d
George Reilly <george@reilly.org>
2023-01-08 01:21:07 -0800

    Clean up tree drawing

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,21 @@ use filetree::*;
 use std::io;
 
 fn print_tree(dir: Directory) {
+    const OTHER_CHILD: &str = "│   ";
+    const OTHER_ENTRY: &str = "├── ";
+    const FINAL_CHILD: &str = "    ";
+    const FINAL_ENTRY: &str = "└── ";
+
     fn visit(node: Directory, prefix: &str) {
         let mut count = node.entries.len();
         for entry in node.entries {
             count -= 1;
-            let line = if count == 0 {
-                "└── " // final
-            } else {
-                "├── " // intermediate
-            };
+            let line = if count == 0 { FINAL_ENTRY } else { OTHER_ENTRY };
             match entry {
                 FileTree::Dir(subdir) => {
                     println!("{}{}{}", prefix, line, subdir.name);
-                    let s = if count == 0 { " " } else { "│" };
-                    visit(subdir, &format!("{}{}   ", prefix, s))
+                    let child = if count == 0 { FINAL_CHILD } else { OTHER_CHILD };
+                    visit(subdir, &format!("{}{}", prefix, child))
                 }
                 FileTree::Symlink(symlink) => {
                     println!("{}{}{} -> {}", prefix, line, symlink.name, symlink.target);
