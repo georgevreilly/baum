955b74a76093d3afbdea1664955df1672b259de4
George V. Reilly <george@reilly.org>
2023-01-08 14:50:24 -0800

    Count dirs and files for tree compatibility

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,9 @@ fn print_tree(dir: Directory) {
     const FINAL_CHILD: &str = "    ";
     const FINAL_ENTRY: &str = "└── ";
 
-    fn visit(node: Directory, prefix: &str) {
+    fn visit(node: Directory, prefix: &str) -> (usize, usize) {
+        let mut dirs: usize = 1; // counting this directory
+        let mut files: usize = 0;
         let mut count = node.entries.len();
         for entry in node.entries {
             count -= 1;
@@ -18,20 +20,26 @@ fn print_tree(dir: Directory) {
                 FileTree::Dir(subdir) => {
                     println!("{}{}{}", prefix, line, subdir.name);
                     let child = if count == 0 { FINAL_CHILD } else { OTHER_CHILD };
-                    visit(subdir, &format!("{}{}", prefix, child))
+                    let (d, f) = visit(subdir, &format!("{}{}", prefix, child));
+                    dirs += d;
+                    files += f;
                 }
                 FileTree::Symlink(symlink) => {
                     println!("{}{}{} -> {}", prefix, line, symlink.name, symlink.target);
+                    files += 1;
                 }
                 FileTree::File(file) => {
                     println!("{}{}{}", prefix, line, file.name);
+                    files += 1;
                 }
             }
         }
+        (dirs, files)
     }
 
     println!("{}", dir.name);
-    visit(dir, "")
+    let (d, f) = visit(dir, "");
+    println!("\n{} directories, {} files", d, f)
 }
 
 fn main() -> io::Result<()> {
