cc34469d55b7881de5cf6b9bca92ac5ec298a62b
George V. Reilly <george@reilly.org>
2023-01-16 23:35:13 -0800

    Clean up print_tree

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,6 @@ use treeprint::*;
 fn main() -> io::Result<()> {
     let root = env::args().nth(1).unwrap_or(".".to_string());
     let dir: Directory = dir_walk(&PathBuf::from(root.clone()), is_not_hidden, sort_by_name)?;
-    print_tree(&root, dir);
+    print_tree(&root, &dir);
     Ok(())
 }
--- src/treeprint.rs
+++ src/treeprint.rs
@@ -1,40 +1,47 @@
 use super::filetree::*;
 
-pub fn print_tree(root: &str, dir: Directory) {
-    const OTHER_CHILD: &str = "│   ";
-    const OTHER_ENTRY: &str = "├── ";
-    const FINAL_CHILD: &str = "    ";
-    const FINAL_ENTRY: &str = "└── ";
+pub fn print_tree(root: &str, dir: &Directory) {
+    const OTHER_CHILD: &str = "│   "; // prefix: pipe
+    const OTHER_ENTRY: &str = "├── "; // connector: tee
+    const FINAL_CHILD: &str = "    "; // prefix: no siblings
+    const FINAL_ENTRY: &str = "└── "; // connector: elbow
 
-    fn visit(node: Directory, prefix: &str) -> (usize, usize) {
+    println!("{}", root);
+    let (d, f) = visit(dir, "");
+    println!("\n{} directories, {} files", d, f);
+
+    fn visit(node: &Directory, prefix: &str) -> (usize, usize) {
         let mut dirs: usize = 1; // counting this directory
         let mut files: usize = 0;
         let mut count = node.entries.len();
-        for entry in node.entries {
+        for entry in &node.entries {
             count -= 1;
-            let line = if count == 0 { FINAL_ENTRY } else { OTHER_ENTRY };
+            let connector = if count == 0 { FINAL_ENTRY } else { OTHER_ENTRY };
             match entry {
-                FileTree::DirNode(subdir) => {
-                    println!("{}{}{}", prefix, line, subdir.name);
-                    let child = if count == 0 { FINAL_CHILD } else { OTHER_CHILD };
-                    let (d, f) = visit(subdir, &format!("{}{}", prefix, child));
+                FileTree::DirNode(sub_dir) => {
+                    println!("{}{}{}", prefix, connector, sub_dir.name);
+                    let new_prefix = format!(
+                        "{}{}",
+                        prefix,
+                        if count == 0 { FINAL_CHILD } else { OTHER_CHILD }
+                    );
+                    let (d, f) = visit(&sub_dir, &new_prefix);
                     dirs += d;
                     files += f;
                 }
                 FileTree::LinkNode(symlink) => {
-                    println!("{}{}{} -> {}", prefix, line, symlink.name, symlink.target);
+                    println!(
+                        "{}{}{} -> {}",
+                        prefix, connector, symlink.name, symlink.target
+                    );
                     files += 1;
                 }
                 FileTree::FileNode(file) => {
-                    println!("{}{}{}", prefix, line, file.name);
+                    println!("{}{}{}", prefix, connector, file.name);
                     files += 1;
                 }
             }
         }
         (dirs, files)
     }
-
-    println!("{}", root);
-    let (d, f) = visit(dir, "");
-    println!("\n{} directories, {} files", d, f)
 }
